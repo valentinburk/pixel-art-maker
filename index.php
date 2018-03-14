a6df96628b4bbb52091ff7d0a9bfb98452a0ed53
Valentin Burkin <burkinman@me.com>
2018-03-14 11:05:46 +0300

    refactor: Move and rename app files

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-  <link href="styles.css" rel="stylesheet">
+  <link href="css/app.css" rel="stylesheet">
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
   <!-- Farbstastic Color Picker -->
-  <link href="farbtastic/farbtastic.css" rel="stylesheet">
-  <script src="farbtastic/farbtastic.js"></script>
+  <link href="lib/farbtastic/farbtastic.css" rel="stylesheet">
+  <script src="lib/farbtastic/farbtastic.js"></script>
 </head>
 
 <body>
@@ -115,7 +115,7 @@
     </div>
   </footer>
 
-  <script src="designs.js"></script>
+  <script src="js/app.js"></script>
 </body>
 
 </html>
